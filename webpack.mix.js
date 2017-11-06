0e39b4ded995bdd0c6634014319d5cf30ec344ac
Diego Vilariño <diego.vilarino@gmail.com>
2017-11-06 13:59:24 +0100

    add browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,5 +10,6 @@ let mix = require('laravel-mix');
 mix.js('app/js/app.js', 'public/static/build')
     .sass('app/styles/app.scss', 'public/static/build')
     .setPublicPath('public/static/build')
+    .browserSync('0.0.0.0:8181')
     .version()
 ;
