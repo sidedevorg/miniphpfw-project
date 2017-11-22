7c9f33591902d06e18f70f09e7299d69363ecfc3
Diego Vilariño <diego.vilarino@gmail.com>
2017-11-22 23:41:54 +0100

    add normalize

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,6 +9,10 @@ let mix = require('laravel-mix');
 
 mix.js('app/js/app.js', 'public/static/build')
     .sass('app/styles/app.scss', 'public/static/build')
+    .combine([
+        'node_modules/normalize.css/normalize.css',
+        'public/static/build/app.css'
+    ], 'public/static/build/app.css')
     .setPublicPath('public/static/build')
     .browserSync('0.0.0.0:8181')
     .version()
